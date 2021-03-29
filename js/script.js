bdb09829c0a8cf27bb28136cb3bd41562c422dbc
Andrei Kireichuk <a.k.kireichuk@gmail.com>
2021-03-29 14:01:45 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+document.getElementById("click-year").innerHTML = "$240 ";
+document.getElementById("click-period").innerHTML = "/YEAR"
+}
+
+function monthFunction() {
+    document.getElementById("click-year").innerHTML = "$24 ";
+    document.getElementById("click-period").innerHTML = "/MONTH"
+}
